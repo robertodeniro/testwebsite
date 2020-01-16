2fb979ec00fa05efcac70cd722eb8f99ed83706f
robertodeniro <59917409+robertodeniro@users.noreply.github.com>
2020-01-16 11:17:49 +0000

    Update readfile.js

--- readfile.js
+++ readfile.js
@@ -1,19 +1,10 @@
+<script>
 var ForReading = 1, ForWriting = 2, ForAppending = 8;
-// The following line contains constants for the OpenTextFile
-// format argument, which is not used in the code below.
-var TristateUseDefault = -2, TristateTrue = -1, TristateFalse = 0;
 
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 
 // Open the file for output.
 var filename = "c:\\windows\\system32\\drivers\\etc\hosts";
-
-var f = fso.OpenTextFile(filename, ForWriting, true);
-
-// Write to the file.
-f.WriteLine("Hello world!");
-f.WriteLine("The quick brown fox");
-f.Close();
 
 // Open the file for input.
 f = fso.OpenTextFile(filename, ForReading);
@@ -25,3 +16,4 @@ while (!f.AtEndOfStream)
     document.write (r + "<br />");
     }
 f.Close();
+</script>
